0fbe790bd0a2807ab9db9b4d4f9abab961b5c3f3
5ham5h33r <Shamsheerkhalid99@gmail.com>
2023-04-07 23:36:30 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Anime_Recommendation_System
+ Anime Recommendation System is a project designed to give users Anime Watching recommendations based on the Anime they have already watched using various Machine Learning Techniques
---
+++ anime_rec_sys.py
@@ -0,0 +1,93 @@
+from sklearn.metrics.pairwise import sigmoid_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import streamlit as st
+
+# Load the anime data and ratings data
+# ratings_path = 'animelist.csv'
+data_path = 'anime.csv'
+# anime_ratings = pd.read_csv(ratings_path)
+anime_data = pd.read_csv(data_path)
+
+# Clean the anime data
+anime_data = anime_data[['MAL_ID', 'Name', 'Score',
+                         'Genres', 'Type', 'Episodes', 'Members']]
+anime_data.rename(columns={'MAL_ID': "anime_id"}, inplace=True)
+anime_data['Name'] = anime_data['Name'].apply(
+    lambda x: re.sub(r'&quot;', '', x))
+anime_data['Name'] = anime_data['Name'].apply(
+    lambda x: re.sub(r'.hack//', '', x))
+anime_data['Name'] = anime_data['Name'].apply(
+    lambda x: re.sub(r'&#039;', '', x))
+anime_data['Name'] = anime_data['Name'].apply(
+    lambda x: re.sub(r'A&#039;s', '', x))
+anime_data['Name'] = anime_data['Name'].apply(
+    lambda x: re.sub(r'I&#039;', 'I\'', x))
+anime_data['Name'] = anime_data['Name'].apply(
+    lambda x: re.sub(r'&amp;', 'and', x))
+anime_data['Genres'] = anime_data['Genres'].fillna('')
+genres_str = anime_data['Genres'].str.split(',').astype(str)
+
+# Initialize the TfidfVectorizer with various parameters
+tfv = TfidfVectorizer(min_df=3, max_features=None,
+                      strip_accents='unicode', analyzer='word',
+                      token_pattern=r'\w{1,}',
+                      ngram_range=(1, 3),
+                      stop_words='english')
+
+# Use the TfidfVectorizer to transform the genres_str into a sparse matrix
+tfv_matrix = tfv.fit_transform(genres_str)
+
+# Compute the sigmoid kernel
+sig = sigmoid_kernel(tfv_matrix, tfv_matrix)
+
+# Create a Pandas Series object where the index is the anime names and the values are the indices in anime_data
+indices = pd.Series(anime_data.index, index=anime_data['Name'])
+indices = indices.drop_duplicates()
+
+# Define the give_rec function to recommend anime based on similarity to input title
+
+
+def give_rec(title, sig=sig):
+    # Get the index corresponding to anime title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores
+    sig_scores = list(enumerate(sig[idx]))
+
+    # Sort the anime based on similarity scores
+    sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the indices of top 10 most similar anime excluding the input anime
+    anime_indices = [i[0] for i in sig_scores[1:11]]
+
+    # Create dataframe of top 10 recommended anime
+    top_anime = pd.DataFrame({
+        'Anime name': anime_data['Name'].iloc[anime_indices].values,
+        'Rating': anime_data['Score'].iloc[anime_indices].values
+    })
+
+    return top_anime
+
+
+# Set up the Streamlit app
+st.title('Anime Recommender System')
+options = anime_data['Name'].tolist()
+options.append('Type name here..')
+# Create a text input box for the user to enter an anime title
+user_input = st.selectbox(
+    'Enter the name of an anime you like:', options=options, index=len(options)-1)
+if user_input == options[-1]:
+    pass
+# When the user submits an input, call the give_rec function and display the output
+elif user_input:
+    try:
+        recommendations = give_rec(user_input)
+        st.write(f"Recommended anime similar to {user_input}:")
+        st.table(recommendations)
+    except KeyError:
+        st.write(f"Sorry, {user_input} is not in our database.")
